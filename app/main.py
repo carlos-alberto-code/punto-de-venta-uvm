c1a8b810ead95b600f0d837645dd83257479f5fb
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-03-16 08:43:04 -0600

    Limpieza del main

--- app/main.py
+++ app/main.py
@@ -1,27 +1,8 @@
 import flet as ft
 
-from views.theme.theme_config import LightTheme, DarkTheme
-from app.views.modules.view_template import SectionView
-
-from views.components.navs import Sidebar
-from views.modules.modules import ModuleView
 
 def main(page: ft.Page):
-    
-    page.theme_mode = ft.ThemeMode.LIGHT
-    page.theme = LightTheme
-    page.bgcolor = LightTheme.color_scheme.background # type: ignore
-    page.adaptive = True
-    
-    section = SectionView(page)
-    section.sections.append(ft.NavigationDrawerDestination(icon=ft.icons.SHOP, label='Compras'))
-    section.sections.append(ft.NavigationDrawerDestination(icon=ft.icons.SHIELD, label='Proveedores'))
-    section.modules.append(ft.NavigationDestination(icon=ft.icons.SHOP, label='Compras'))
-    section.modules.append(ft.NavigationDestination(icon=ft.icons.SHIELD, label='Proveedores'))
-    page.views.append(section.build())
-    
-    page.update()
+    pass
 
-    
 
 ft.app(target=main)
