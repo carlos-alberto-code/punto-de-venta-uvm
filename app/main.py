80d07e9085d3e3164dcaa8f3e339aad311fa6db7
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-03-23 12:16:28 -0600

    Mejora en la navegación. Ahora ya inicializa las secciones de un módulo, cambia el título conforma cambia el módulo, y siempre selecciona el primer control de sección del moudlo

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import flet as ft
 from view.navigation import active_navs
 
 def main(page: ft.Page):
-    
+
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.bgcolor = ft.colors.WHITE
     page.navigation_bar, page.drawer, page.appbar = active_navs()
     page.update()
 
--- app/view/navigation.py
+++ app/view/navigation.py
@@ -10,15 +10,13 @@ class _DrawerControls:
     def get_top_controls() -> List[ft.Control]:
         return [
             ft.Container(height=14),
-            ft.NavigationDrawerDestination(label='Carlos Alberto', icon=ft.icons.CIRCLE),
+            ft.NavigationDrawerDestination(label='Carlos Alberto', icon=ft.icons.VERIFIED_USER),
             ft.Divider()
         ]
     
     @staticmethod
-    def get_middle_controls(
-        section_controls: List[ft.NavigationDrawerDestination]
-    ) -> List[ft.NavigationDrawerDestination]:
-        return section_controls
+    def get_middle_controls(section_controls: List[ft.NavigationDrawerDestination]) -> List[ft.Control]:
+        return [*section_controls]
 
     @staticmethod
     def get_bottom_controls() -> List[ft.Control]:
@@ -30,6 +28,9 @@ class _DrawerControls:
 
 
 # EVENT HANDLER
+    
+SELECTED_MODULE = 0
+SELECTED_SECTION = 1
 
 # Principal functions
     
@@ -47,6 +48,7 @@ def _update_drawer_sections(module):
         *_DrawerControls.get_middle_controls(sections),
         *_DrawerControls.get_bottom_controls(),
     ]
+    _drawer.selected_index = SELECTED_SECTION
     _drawer.open = True
     _drawer.update()
 
@@ -70,15 +72,30 @@ def _get_and_build_sections(module) -> List[ft.NavigationDrawerDestination]:
     return [section.build() for section in module.sections]
 
 
+# Initial functions
+
+def _init_drawer():
+    module = _get_module(SELECTED_MODULE) # Módulo seleccionado por defecto
+    sections = _get_and_build_sections(module)
+    _drawer.controls = [
+        *_DrawerControls.get_top_controls(),
+        *_DrawerControls.get_middle_controls(sections),
+        *_DrawerControls.get_bottom_controls(),
+    ]
+    _drawer.selected_index = SELECTED_SECTION
+    _drawer.open = True
+    _appbar.title = ft.Text(module.name)
+
 # Implementation
 
 
 _modules = create_modules()
 _navbar = ft.NavigationBar(
+    selected_index=SELECTED_MODULE,
     destinations=_get_and_build_destinations(),
     on_change=_update,
 )
-_drawer = ft.NavigationDrawer(controls=[])
+_drawer = ft.NavigationDrawer(controls=[], selected_index=0, open=False)
 _appbar = ft.AppBar(
     title=ft.Text('Aplicación'),
     center_title=True,
@@ -88,8 +105,30 @@ _appbar = ft.AppBar(
         ft.IconButton(icon=ft.icons.CLOSE),
     ]
 )
+_init_drawer()
 
 # Export
 
 def active_navs():
     return _navbar, _drawer, _appbar
+
+
+
+# TODO: Fix the following issues:
+"""
+Si aparece cómo título en el Appbar 'Aplicación', significa que no hay secciones en el Drawer.
+Eso no debería pasar, porque el primer módulo debería ser seleccionado por defecto junto 
+con la primer sección del Drawer para mostrar contenido.
+
+Otro problema es que, al navegar entre módulos, el Drawer mantiene como selección el control anterior.
+Esto no debería pasar, porque al cambiar de módulo, el Drawer debería seleccionar el primer control.
+
+Para ello debemos hacer:
+- Que el Drawer seleccione el primer control (perteneciente al módulos) al cambiar de módulo.
+- Que el Drawer se inicialice con el primer módulo seleccionado y su primer control activo.
+
+Una mejora adicional para mejorar la retroalimentación para el usuario sobre en qué módulo se encuentra
+es cambiar el color de fondo del Drawer y el Appbar para que coincidan con el color del módulo seleccionado.
+Eso implicaría que cada módulo tenga un color de fondo asociado y relacionado con todos los componentes 
+de navegación. Por ahora esa mejora no es prioritaria, pero es algo a tener en cuenta para mejorar la UX.
+"""
