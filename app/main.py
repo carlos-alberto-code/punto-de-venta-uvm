eddcd64eaab92ce6856dd68f39e3f6579188af3b
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-03-26 16:26:11 -0600

    Mejora en la arquitectura del appbar

--- app/main.py
+++ app/main.py
@@ -1,24 +1,19 @@
 import flet as ft
 
-from view.navigation.appbar_controls import theme_button, close_button
+from view.navigation.appbar_controls import AppbarActions
+from view.navigation.appbar import Appbar
 # from view.modules.modules_declaration import module
 
 def main(page: ft.Page):
 
-    # Set the initial theme based on user preference
-    page.theme_mode = ft.ThemeMode.DARK  # This should be replaced with the user's preferred theme
+    page.theme_mode = ft.ThemeMode.DARK  # Se remplaza por las preferencias de usuario
 
-    page.theme_mode = page.session.get('theme_mode')
-    theme_button.icon = ft.icons.LIGHT_MODE if page.theme_mode == ft.ThemeMode.DARK else ft.icons.DARK_MODE
+    # Appbar: Construcción inicial del appbar
+    appbar_actions = AppbarActions(page.theme_mode)
+    appbar = Appbar(actions=appbar_actions.controls).build()
+    page.appbar = appbar
 
-    # Appbar
-    page.appbar = ft.AppBar(
-        title=ft.Text('Appbar title'),
-        actions=[
-            theme_button,
-            close_button
-        ]
-    )
     page.update()
+
 
 ft.app(target=main)
--- app/view/navigation/appbar_controls.py
+++ app/view/navigation/appbar_controls.py
@@ -1,30 +1,44 @@
 from typing import List
 import flet as ft
 
+
 # Icons
 _light_mode  = ft.icons.LIGHT_MODE
 _dark_mode   = ft.icons.DARK_MODE
 _close       = ft.icons.CLOSE
 
 
-def _toggle_theme(event: ft.ControlEvent):
-    # Toggle the theme
-    event.page.theme_mode = ft.ThemeMode.DARK if event.page.theme_mode == ft.ThemeMode.LIGHT else ft.ThemeMode.LIGHT
-    # Update the icon to match the new theme
-    event.control.icon = _light_mode if event.page.theme_mode == ft.ThemeMode.DARK else _dark_mode
-    # Update the page to apply the new theme
-    event.page.update()
+class AppbarActions:
 
-theme_button = ft.IconButton(icon=_light_mode, on_click=_toggle_theme)
+    def __init__(self, theme_mode: ft.ThemeMode) -> None:
+        self.theme_mode = theme_mode
+        self.theme_button = ft.IconButton(icon=self._get_theme_icon(), on_click=self._toggle_theme)
+        self.close_button = ft.IconButton(icon=_close, on_click=self._close_app)
+    
+    @property
+    def controls(self) -> List[ft.Control]:
+        return [self.theme_button, self.close_button]
 
+    def _get_theme_icon(self) -> str:
+        return _light_mode if self.theme_mode == ft.ThemeMode.DARK else _dark_mode
+    
+    def _toggle_theme(self, event: ft.ControlEvent):
+        self.theme_mode = ft.ThemeMode.DARK if self.theme_mode == ft.ThemeMode.LIGHT else ft.ThemeMode.LIGHT
+        self.theme_button.icon = self._get_theme_icon()
+        event.page.theme_mode = self.theme_mode
+        event.page.update()
+    
+    def _close_app(self, event: ft.ControlEvent):
+        event.page.window_close()
 
-def _close_app(event: ft.ControlEvent):
-    event.page.window_close()
-
-close_button = ft.IconButton(icon=_close, on_click=_close_app)
 
 
 """
 NOTE: En caso de necesitarse más controles, este es el lugar en el que se declaran,
-junto con sus eventos.
+junto con sus eventos. Es posible en un futuro crear una estructura de datos que pueda
+encapsular todos los controles.
+
+EXPLICACION: Se buscó separar la lógica de los controles de la barra de aplicaciones en
+en este archivo para mantener el código más limpio y organizado. La clase AppbarActions
+se encarga de sostener estos controles y manejar los eventos correspondientes.
 """
