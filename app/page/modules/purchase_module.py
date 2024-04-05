5a3eb241c8075de08f523c471130bd6791e8efa6
unknown <jymontes13@gmail.com>
2024-04-05 17:10:03 -0600

    Seccion de compras añadida

--- app/page/modules/purchase_module.py
+++ app/page/modules/purchase_module.py
@@ -1,5 +1,6 @@
 from ..navigation.builder import Module, Section
 from .content.purchases.suppliers_section import content
+from .content.purchases.purchase_section import PurchaseSection
 
 
 import flet as ft
@@ -8,5 +9,6 @@ import flet as ft
 Module(
     'Compras',
     ft.icons.SHOPPING_BASKET,
-    Section('Proveedores', ft.icons.ABC, content)
+    #Section('Proveedores', ft.icons.ABC, content),
+    Section('Entradas',ft.icons.LIBRARY_ADD_CHECK_ROUNDED,PurchaseSection)
 )
