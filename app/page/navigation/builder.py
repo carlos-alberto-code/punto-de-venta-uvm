e7b9d078f8baba279f536051c7a5d89b2e2e72a0
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-04-17 22:59:30 -0600

    Refactirización de los objetos de Module por la nueva versión

--- app/page/navigation/builder.py
+++ app/page/navigation/builder.py
@@ -2,50 +2,15 @@ from typing import List
 import flet as ft
 
 
-class Rail(ft.UserControl):
-    """
-    Un ``Rail`` es una clase que representa una opción en un menú de navegación global.
-    Se implementa en el ``ft.NavigationBar`` en forma de ``ft.NavigationDestination``.
-
-    #### Args:
-    - ``name (str)``: El nombre del módulo (``ft.NavigationDestination``).
-    - ``icon (str)``: El ícono que representa el módulo (``ft.NavigationDestination``).
-
-    No es necesario el uso de accesores, sólo se debe llamar al método ``build``  cuando 
-    se desempaquete del objeto ``Module`` para obtener el objeto ``ft.NavigationDestination``
-    que representa el módulo en el ``ft.NavigationBar``.
-    """
+class Rail(ft.NavigationDestination):
 
     def __init__(self, name: str, icon: str) -> None:
         super().__init__()
-        self._name = name
-        self._icon = icon
-    
-    
-    def build(self) -> ft.NavigationDestination:
-        return ft.NavigationDestination(
-            label=self._name,
-            icon=self._icon,
-        )
+        self.label = name
+        self.icon = icon
     
 
-class Section(ft.UserControl):
-    """
-    Los módulos tienen diferentes secciones que representan las funcionalidades específicas.
-    Una sección es sólo una opción en un menú de navegación ``ft.NavigationDrawer``.
-
-    Dependiendo de la robustes de la aplicación, pueden declararse las secciones dentro del módulo
-    o residir en archivos en los que se cree el contenido y se inyecte en la sección, para después
-    inyectarse en el módulo.
-
-    #### Args:
-    - ``name (str)``: El nombre de la sección.
-    - ``icon (str)``: El ícono que representa la sección.
-    - ``content (ft.Column)``: El contenido de la sección. Por defecto es un texto que dice 'None'.
-    El content se establece como un texto por defecto para que el desarrollador pueda identificar
-    que la sección no tiene contenido. Además de eso, permite que el dev pueda ver inmediatamente
-    su progreso sin que la falta de contenido impida la visualización de la sección.
-    """
+class Section(ft.NavigationDrawerTheme):
     
     def __init__(
             self,
