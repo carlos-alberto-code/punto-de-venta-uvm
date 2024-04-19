df98a2f6d72cf34051f2d29ab95b7209af9c7c59
unknown <jymontes13@gmail.com>
2024-04-19 16:48:43 -0600

    Tabla de inventario provisional agregada

--- app/page/modules/content/inventory/stock_section.py
+++ app/page/modules/content/inventory/stock_section.py
@@ -1,9 +1,52 @@
 import flet as ft
+from controllers.product_contrller import ProductController
 
+_title = ft.Text('Inventario', size=25)
+_table = ft.DataTable(
+    sort_column_index=0,
+    sort_ascending=False,
+    columns=[
+        ft.DataColumn(ft.Text('sku')),
+        ft.DataColumn(ft.Text('Unidad ID')),
+        ft.DataColumn(ft.Text('Categoria ID')),
+        ft.DataColumn(ft.Text('Marca ID')),
+        ft.DataColumn(ft.Text('Cantidad')),
+        ft.DataColumn(ft.Text('Precio de compra')),
+        ft.DataColumn(ft.Text('Precio de venta')),
+        ft.DataColumn(ft.Text('Stock minimo')),
+        ft.DataColumn(ft.Text('Descripcion')),
+    ],
+    rows=[
+        ft.DataRow(
+            cells=[
+                ft.DataCell(ft.Text(str(product.sku))),
+                ft.DataCell(ft.Text(str(product.unit_id))),
+                ft.DataCell(ft.Text(str(product.category_id))),
+                ft.DataCell(ft.Text(str(product.brand_id))),
+                ft.DataCell(ft.Text(str(product.quantity))),
+                ft.DataCell(ft.Text(str(product.purchase_price))),
+                ft.DataCell(ft.Text(str(product.sale_price))),
+                ft.DataCell(ft.Text(str(product.minimum_stock))),
+                ft.DataCell(ft.Text(product.description)),
+            ]
+        )for product in ProductController().get_all()
+    ]
+)
 
-class StockSection(ft.Column):
-
-    def __init__(self):
-        super().__init__()
-    
+StockSection = ft.Column(
+    scroll=ft.ScrollMode.ALWAYS,
+    controls=[
+        ft.Row(
+            [
+                ft.Icon(name=ft.icons.INVENTORY),
+                _title
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
+        ft.Row(
+            [_table],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    ]
+)
     
