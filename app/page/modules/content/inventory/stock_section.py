46bed0145d8c866ee64993f50b9e769544206de3
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-04-29 20:41:35 -0600

    Implementación de los eventos de ordenacion para todas las columnas excepto la columna de descripcion. Se pretende implementar que en el mismo clic, se invierta el orden, es decir, si está ordenado de la A-Z al siguiente clic ordene al revés, si se da otro clic, vuelve a ordenar de A-Z es como un switch. Para el caso de las columnas numericas debe funcionar igual.

--- app/page/modules/content/inventory/stock_section.py
+++ app/page/modules/content/inventory/stock_section.py
@@ -1,59 +1,6 @@
 import flet as ft
-from controllers.inventory_controller import InventoryController
 
-# Carlos: Implementación
 from controllers.products_controller import ProductController
-
-_title = ft.Text('Inventario', size=25)
-_table = ft.DataTable(
-    sort_column_index=0,
-    sort_ascending=False,
-    columns=[
-        ft.DataColumn(ft.Text('sku')),
-        ft.DataColumn(ft.Text('Unidad ID')),
-        ft.DataColumn(ft.Text('Categoria ID')),
-        ft.DataColumn(ft.Text('Marca ID')),
-        ft.DataColumn(ft.Text('Cantidad')),
-        ft.DataColumn(ft.Text('Precio de compra')),
-        ft.DataColumn(ft.Text('Precio de venta')),
-        ft.DataColumn(ft.Text('Stock minimo')),
-        ft.DataColumn(ft.Text('Descripcion')),
-    ],
-    rows=[
-        ft.DataRow(
-            cells=[
-                ft.DataCell(ft.Text(str(product.sku))),
-                ft.DataCell(ft.Text(str(product.unit_id))),
-                ft.DataCell(ft.Text(str(product.category_id))),
-                ft.DataCell(ft.Text(str(product.brand_id))),
-                ft.DataCell(ft.Text(str(product.quantity))),
-                ft.DataCell(ft.Text(str(product.purchase_price))),
-                ft.DataCell(ft.Text(str(product.sale_price))),
-                ft.DataCell(ft.Text(str(product.minimum_stock))),
-                ft.DataCell(ft.Text(product.description)),
-            ]
-        )for product in InventoryController().get_all()
-    ]
-)
-
-stock_section = ft.Column(
-    scroll=ft.ScrollMode.ALWAYS,
-    controls=[
-        ft.Row(
-            [
-                ft.Icon(name=ft.icons.INVENTORY),
-                _title
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
-        ft.Row(
-            [_table],
-            alignment=ft.MainAxisAlignment.CENTER
-        )
-    ]
-)
-
-# Carlos: Implementación
 
 
 _search_bar = ft.TextField(
@@ -75,6 +22,17 @@ class StockSection(ft.Column):
     def __init__(self):
         super().__init__()
         self.products = ProductController.get_all()
+        self.spacing = 20
+        self.sort_functions = {
+            'SKU': lambda: self.sort_by('sku', int),
+            'UNIDAD': lambda: self.sort_by('unit', lambda x: str(x).lower()),
+            'CATEGORÍA': lambda: self.sort_by('category', str),
+            'MARCA': lambda: self.sort_by('brand', str),
+            'CANTIDAD': lambda: self.sort_by('quantity', int),
+            'PRECIO DE COMPRA': lambda: self.sort_by('purchase_price', float),
+            'PRECIO DE VENTA': lambda: self.sort_by('sale_price', float),
+            'STOCK MÍNIMO': lambda: self.sort_by('minimum_stock', int),
+        }
         self.controls = [
             ft.Row(
                 alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
@@ -85,14 +43,14 @@ class StockSection(ft.Column):
             ), # Row End
             ft.DataTable(
                 columns=[
-                    ft.DataColumn(ft.TextButton('SKU')),
-                    ft.DataColumn(ft.TextButton('UNIDAD')),
-                    ft.DataColumn(ft.TextButton('CATEGORÍA')),
-                    ft.DataColumn(ft.TextButton('MARCA')),
-                    ft.DataColumn(ft.TextButton('CANTIDAD')),
-                    ft.DataColumn(ft.TextButton('PRECIO DE COMPRA')),
-                    ft.DataColumn(ft.TextButton('PRECIO DE VENTA')),
-                    ft.DataColumn(ft.TextButton('STOCK MÍNIMO')),
+                    ft.DataColumn(ft.TextButton('SKU', on_click=self.sort_by_sku)),
+                    ft.DataColumn(ft.TextButton('UNIDAD', on_click=self.sort_by_unit)),
+                    ft.DataColumn(ft.TextButton('CATEGORÍA', on_click=self.sort_by_category)),
+                    ft.DataColumn(ft.TextButton('MARCA', on_click=self.sort_by_brand)),
+                    ft.DataColumn(ft.TextButton('CANTIDAD', on_click=self.sort_by_quantity)),
+                    ft.DataColumn(ft.TextButton('PRECIO DE COMPRA', on_click=self.sort_by_purchase_price)),
+                    ft.DataColumn(ft.TextButton('PRECIO DE VENTA', on_click=self.sort_by_sale_price)),
+                    ft.DataColumn(ft.TextButton('STOCK MÍNIMO', on_click=self.sort_by_minimum_stock)),
                     ft.DataColumn(ft.TextButton('DESCRIPCIÓN')),
                 ],
                 rows=[
@@ -113,3 +71,57 @@ class StockSection(ft.Column):
                 ]
             )
         ]
+
+    def sort_by(self, attribute, type):
+        self.products = sorted(self.products, key=lambda product: type(getattr(product, attribute)))
+        self.update_table()
+
+    def update_table(self):
+        self.controls[1].rows = [
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(str(product.sku))),
+                    ft.DataCell(ft.Text(str(product.unit))),
+                    ft.DataCell(ft.Text(str(product.category))),
+                    ft.DataCell(ft.Text(str(product.brand))),
+                    ft.DataCell(ft.Text(str(product.quantity))),
+                    ft.DataCell(ft.Text(str(product.purchase_price))),
+                    ft.DataCell(ft.Text(str(product.sale_price))),
+                    ft.DataCell(ft.Text(str(product.minimum_stock))),
+                    ft.DataCell(ft.Text(product.description)),
+                ]
+            ) for product in self.products
+        ]
+
+    def sort_by_sku(self, event: ft.ControlEvent):
+        self.sort_functions['SKU']()
+        event.page.update()
+
+    def sort_by_unit(self, event: ft.ControlEvent):
+        self.sort_functions['UNIDAD']()
+        event.page.update()
+
+    def sort_by_category(self, event: ft.ControlEvent):
+        self.sort_functions['CATEGORÍA']()
+        event.page.update()
+    
+    def sort_by_brand(self, event: ft.ControlEvent):
+        self.sort_functions['MARCA']()
+        event.page.update()
+    
+    def sort_by_quantity(self, event: ft.ControlEvent):
+        self.sort_functions['CANTIDAD']()
+        event.page.update()
+    
+    def sort_by_purchase_price(self, event: ft.ControlEvent):
+        self.sort_functions['PRECIO DE COMPRA']()
+        event.page.update()
+
+    def sort_by_sale_price(self, event: ft.ControlEvent):
+        self.sort_functions['PRECIO DE VENTA']()
+        event.page.update()
+    
+    def sort_by_minimum_stock(self, event: ft.ControlEvent):
+        self.sort_functions['STOCK MÍNIMO']()
+        event.page.update()
+    
--- app/page/modules/inventory_module.py
+++ app/page/modules/inventory_module.py
@@ -1,5 +1,5 @@
 from ..navigation.module import Module, Section
-from .content.inventory.stock_section import stock_section, StockSection as St
+from .content.inventory.stock_section import StockSection
 # from .content.inventory.reports_section import PurchaseSection
 import flet as ft
 
@@ -7,6 +7,6 @@ import flet as ft
 Module(
     'Inventario',
     ft.icons.ADD_BOX,
-    Section(label='Stock', icon=ft.icons.STORAGE, content=St()),
+    Section(label='Stock', icon=ft.icons.STORAGE, content=StockSection()),
     Section(label='Reportes', icon=ft.icons.INSERT_CHART),
 )
