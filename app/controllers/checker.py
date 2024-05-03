b153a72c2d04935419f81ffaaa2d0120b92f151b
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-05-02 23:58:50 -0600

    Mejora en el controlador de unidades

--- app/controllers/checker.py
+++ app/controllers/checker.py
@@ -1,18 +1,16 @@
 from typing import Any
-
+from sqlalchemy.orm import Session
 from database.connection import get_db
 from models.models import Base
 
 
 class Checker:
 
-    def exist_data_in(self, model: Any) -> bool:
-        with get_db() as db:
-            return db.query(model).first() is not None
+    def exist_data_in(self, model: Any, db: Session) -> bool:
+        return db.query(model).first() is not None
     
-    def name_exist(self, name: str, model: Any) -> bool:
-        with get_db() as db:
-            return db.query(model).filter_by(name=name).first() is not None
+    def name_exist(self, name: str, model: Any, db: Session) -> bool:
+        return db.query(model).filter_by(name=name).first() is not None
 
 
 def validate_name(func):
--- app/controllers/database_exceptions.py
+++ app/controllers/database_exceptions.py
@@ -2,7 +2,10 @@ class DataException(Exception):
     def __init__(self, tablename: str):
         super().__init__(f'There is no data in the table "{tablename}".')
 
-
 class UnitAlreadyExists(Exception):
     def __init__(self, unit_name: str):
         super().__init__(f'The unit "{unit_name.lower()}" already exists in the table "units".')
+
+class UnitNotFoundException(Exception):
+    def __init__(self, unit_name: str):
+        super().__init__(f'The unit "{unit_name.lower()}" does not exist in the table "units".')
