50b1a028501669c8fd0e045f70b896b628ede6af
unknown <jymontes13@gmail.com>
2024-05-05 10:04:44 -0600

    Script para insertar datos en la tabla proveedores

--- app/test/script_insert_suppliers.py
+++ app/test/script_insert_suppliers.py
@@ -1,16 +1,22 @@
-import sys
+import sys, random
 sys.path.append('..')
 
 from models.models import Supplier
-from app.database.connection import get_db
+from database.connection import get_db
 
 suppliers_data = ['The Coca-Cola Company', 'Grupo Bimbo', 'Zwan Food Group', 'Grupo Jumex', 'PepsiCo', 'Unilever', 'Grupo Herdez', 'Nestlé', 'La Costeña', 'Colgate-Palmolive', 'Procter & Gamble (P&G)', 'Johnson & Johnson']
 
 #Insertar datos en la tabla proveedores
 with get_db() as db:
-    for supplier in suppliers_data:
-        if not db.query(Supplier).filter(Supplier.name == supplier).first():
-            db.add(Supplier(name=supplier))
-            db.commit()
-
-print('Datos insertados correctamente')
+    if not db.query(Supplier).first():
+        for i in range(len(suppliers_data)):
+            supplier = Supplier(
+                id=i+1,
+                name=suppliers_data[i],
+                phone=random.randint(1000000000,9999999999)
+            )
+            db.add(supplier)
+        db.commit()
+        print('Datos insertados correctamente')
+    else:
+        print('Ya existen proveedores en la base de datos')
