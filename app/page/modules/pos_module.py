6c318c878ef04f51f7934da719bf1119db769672
unknown <jymontes13@gmail.com>
2024-05-05 11:45:05 -0600

    Seccion de prueba de punto de venta

--- app/page/modules/pos_module.py
+++ app/page/modules/pos_module.py
@@ -1,8 +1,9 @@
 from ..navigation.module import Module, Section
 import flet as ft
-
+from .content.sellPoint.sales_section import SalesSection
 
 Module(
     'Punto de venta',
     ft.icons.SHOP,
+    Section('Ventas',ft.icons.SHOPPING_BAG,content=SalesSection())
 )
