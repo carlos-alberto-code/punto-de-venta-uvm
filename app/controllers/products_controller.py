3018cbc1baaf2193615f9f0c88cb23eba6fa9d41
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-05-13 12:56:11 -0600

    Nuevos metodos para los controladores, ahora es posible filtar los productos por algun tipo de propiedad como marca, categoria o unidad.

--- app/controllers/products_controller.py
+++ app/controllers/products_controller.py
@@ -6,6 +6,17 @@ from database.models import Product, Category, Brand, Unit
 
 
 class ProductController:
+    """
+    Este controlador se encarga de manejar las operaciones relacionadas con los productos.
+    Puede hacer las siguientes operaciones:
+    - Obtener todos los productos.
+    - Obtener un producto por su ID.
+    - Filtrar productos por categoría
+    - Filtrar productos por marca
+    - Filtrar productos por unidad
+    - Buscar productos por una cadena de búsqueda (Aún no implementado)
+    - Actualizar los datos de un producto.
+    """
 
     def _product_query(self):
         with get_db() as db:
@@ -43,3 +54,20 @@ class ProductController:
     
     def get_by_id(self, product_id):
         return self._product_query().filter(Product.sku == product_id).first()
+    
+    def filter_by_category(self, category_id):
+        return self._product_query().filter(Category.id == category_id).all()
+    
+    def filter_by_brand(self, brand_id):
+        return self._product_query().filter(Brand.id == brand_id).all()
+    
+    def filter_by_unit(self, unit_id):
+        return self._product_query().filter(Unit.id == unit_id).all()
+    
+    def update_product(self, product_id, **kwargs):
+        with get_db() as db:
+            product = db.query(Product).filter(Product.sku == product_id).first()
+            for key, value in kwargs.items():
+                setattr(product, key, value)
+            db.commit()
+            return product
