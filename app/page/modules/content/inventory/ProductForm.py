cdfa27e1aa788b4303213f4bc74d53466c4821ed
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-05-22 18:20:14 -0600

    Mejora en los componentes del product form

--- app/page/modules/content/inventory/ProductForm.py
+++ app/page/modules/content/inventory/ProductForm.py
@@ -1,73 +1,21 @@
+from turtle import st
 import flet as ft
 
 from page.components.Counter import Counter
-from page.components.search_bar_filter.filter_interface import IFilter
-
-
-
-class Results(ft.Card):
-    def __init__(self, id:int, text: str, on_click=None) -> None:
-        super().__init__()
-        self.id = id
-        self.text = text
-        self.content = ft.Container(
-            height=30,
-            margin=3,
-            padding=ft.Padding(left=10, right=0, top=0, bottom=0),
-            alignment=ft.alignment.center,
-            ink=True,
-            border_radius=10,
-            content=ft.Row(
-                controls=[
-                    ft.Icon(str(ft.icons.CIRCLE), size=10),
-                    ft.Text(value=text),
-                    ft.Text(value=str(id), visible=False)
-                ],
-                alignment=ft.MainAxisAlignment.START
-            ),
-            on_click=on_click,
-        )
-
-
-class ProductPropertySearch(ft.SearchBar):
-    def __init__(self, bar_hint_text: str, model: IFilter):
-        super().__init__()
-        self.width = 300
-        self.height = 40
-        self.bar_leading = ft.Text(bar_hint_text + ':')
-        self.view_leading = ft.Text(bar_hint_text + ':', color=ft.colors.PRIMARY)
-        self.bar_trailing = [ft.Icon(str(ft.icons.ARROW_DROP_DOWN_CIRCLE_OUTLINED))]
-        self.view_trailing = [ft.IconButton(icon=ft.icons.CLOSE, on_click=lambda e: self.close_view())]
-        self.model = model
-        self.instances = model.get_all()
-        self.controls = [
-            Results(id=instance.id, text=instance.name, on_click=self.on_click_in_result)
-            for instance in self.instances
-            # ft.ElevatedButton(text='Nuevo', on_click=lambda _: print('nueva propiedad'))
-        ]
-        self.controls.append(ft.ElevatedButton(text='Nuevo', on_click=lambda _: print('nueva propiedad'), bgcolor=ft.colors.ON_BACKGROUND, color=ft.colors.BACKGROUND))
-        self.controls.append(ft.Container(height=15))
-        
-    
-    def on_click_in_result(self, event):
-        value = event.control.content.controls[1].value.capitalize()
-        self.bar_leading.color = ft.colors.PRIMARY # type: ignore
-        self.view_leading.color = ft.colors.PRIMARY # type: ignore
-        self.close_view(text=value)
+from page.components.Searcher import Searcher
+from page.components.search_bar_filter.properties_controller import UnitFilter, CategoryFilter, BrandFilter
 
 
 ft.AlertDialog()
 class ProductForm(ft.AlertDialog):
 
-    def __init__(self, **models: IFilter):
+    def __init__(self):
         super().__init__()
-        self.expandable = True
         self.title = ft.Text('Nuevo producto')
         self.icon = ft.Icon(str(ft.icons.INVENTORY))
         self.scrollable = True
         self.content = ft.Column(
             controls=[
-                # Un contenedor para clickear y poder subir una imagen del producto
                 ft.Row(
                     [
                         ft.Container(
@@ -84,37 +32,36 @@ class ProductForm(ft.AlertDialog):
                     ],
                     alignment=ft.MainAxisAlignment.CENTER
                 ),
-                ProductPropertySearch(list(models.keys())[0], list(models.values())[0]),
-                ProductPropertySearch(list(models.keys())[1], list(models.values())[1]),
-                ProductPropertySearch(list(models.keys())[2], list(models.values())[2]),
+                Searcher('Unidad', UnitFilter()),
+                Searcher('Categoría', CategoryFilter()),
+                Searcher('Marca', BrandFilter()),
                 ft.Row(
                     [
                         ft.Text('Cantidad:'),
-                        Counter(0),
+                        Counter(start_value=1),
                     ],
-                    width=300
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN
                 ),
                 ft.Row(
                     [
                         ft.Text('Precio de compra:'),
-                        Counter(0),
+                        Counter(start_value=1),
                     ],
-                    width=300
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN
                 ),
                 ft.Row(
                     [
                         ft.Text('Precio de venta:'),
-                        Counter(0),
+                        Counter(start_value=1),
                     ],
-                    width=300,
                     alignment=ft.MainAxisAlignment.SPACE_BETWEEN
                 ),
                 ft.Row(
                     [
                         ft.Text('Existencias mínimas:'),
-                        Counter(0),
+                        Counter(start_value=1),
                     ],
-                    width=300
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN
                 ),
             ],
         )
