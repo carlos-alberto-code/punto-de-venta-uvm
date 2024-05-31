4b59cc237dc5a8b2c875f3dbc40a7de484bcc123
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-05-31 15:41:50 -0600

    Estable: Inicio para la refactorizacion de inyecion de eventos en todos los componentes

--- app/page/modules/content/inventory/ProductForm.py
+++ app/page/modules/content/inventory/ProductForm.py
@@ -8,29 +8,13 @@ from controllers.controllers import UnitController, CategoryController, BrandCon
 ft.AlertDialog()
 class ProductForm(ft.AlertDialog):
 
-    def __init__(self):
+    def __init__(self, on_save_click=None):
         super().__init__()
         self.title = ft.Text('Nuevo producto')
         self.icon = ft.Icon(str(ft.icons.INVENTORY))
         self.scrollable = True
         self.content = ft.Column(
             controls=[
-                # ft.Row(
-                #     [
-                #         ft.Container(
-                #             height=100,
-                #             width=100,
-                #             margin=10,
-                #             border=ft.border.all(0.5),
-                #             border_radius=10,
-                #             ink=True,
-                #             alignment=ft.alignment.center,
-                #             content=ft.Icon(str(ft.icons.IMAGE), size=50),
-                #             on_click=lambda _: print('Subir imagen'),
-                #         )
-                #     ],
-                #     alignment=ft.MainAxisAlignment.CENTER
-                # ),
                 Searcher(UnitController()),
                 Searcher(CategoryController()),
                 Searcher(BrandController()),
@@ -66,5 +50,5 @@ class ProductForm(ft.AlertDialog):
         )
         self.actions = [
             ft.ElevatedButton(text='Limpiar', on_click=lambda _: print('Limpiar')),
-            ft.ElevatedButton(text='Guardar', on_click=lambda _: print('Guardar')),
+            ft.ElevatedButton(text='Guardar', on_click=on_save_click),
         ]
--- app/page/modules/content/inventory/stock_section.py
+++ app/page/modules/content/inventory/stock_section.py
@@ -1,9 +1,10 @@
 import flet as ft
 
+from page.modules.content.inventory.ProductForm     import ProductForm
 from page.modules.content.inventory.ProductTable    import ProductTable
 from page.modules.content.inventory.searchers       import ProductSearcher
-from page.modules.content.inventory.top_buttons     import OptionsMenuButton
 from controllers.controllers                        import ProductController
+from page.modules.content.inventory.OptionButtons   import OptionsMenuButton, AddProductButton
 
 
 class StockSection(ft.Column):
@@ -14,17 +15,27 @@ class StockSection(ft.Column):
             # scroll=ft.ScrollMode.AUTO,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         )
+        # Delcaración de los botones opcionales
+        self.add_product_button = AddProductButton(on_click=self.handler_on_click_add_product)
+        self.options_menu_button = OptionsMenuButton(self.add_product_button)
+
+        # Declaración de los componentes de la sección de inventario
+        self.product_searcher = ProductSearcher(on_change=self.handle_on_change_event, on_tap=self.handle_on_tap_event)
+        self.product_form = ProductForm(
+            on_save_click=self.handler_on_click_save_product
+        )
+
+
         self.product_controller = ProductController()
         products = self.product_controller.get_all()
         self.product_table = ProductTable(products)
-        self.product_searcher = ProductSearcher(on_change=self.handle_on_change_event, on_tap=self.handle_on_tap_event)
         self.controls = [
             ft.Row( # SearchBarFilter, MenuOptionsButton, Container
                 [   
                     
                     # Searcher('Productos', BrandFilter()),
                     self.product_searcher,
-                    OptionsMenuButton(),
+                    self.options_menu_button,
                     ft.Container(width=45)
                 ],
                 alignment=ft.MainAxisAlignment.END,
@@ -38,15 +49,27 @@ class StockSection(ft.Column):
             )
         ]
     
+    # Maneja el evento de cambio en el buscador
     def handle_on_change_event(self, event: ft.ControlEvent):
         control = event.control
         search_term = control.value
         results = self.product_controller.search(search_term)
         self.product_table.products = results
         
+    # Maneja el evento de toque en el buscador
     def handle_on_tap_event(self, event: ft.ControlEvent):
         control = event.control
         search_term = control.value
         control.close_view()
         self.product_table.products = self.product_controller.get_all()
+
+    # Evento para abrir el formulario de agregar producto
+    def handler_on_click_add_product(self, event: ft.ControlEvent):
+        event.page.dialog = self.product_form
+        event.page.dialog.open = True
+        event.page.update()
+    
+    # Evento para guardar los datos del formulario
+    def handler_on_click_save_product(self, event: ft.ControlEvent):
+        print(event.page.dialog.data)
         
