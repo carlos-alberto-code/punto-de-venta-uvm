15737aa2e5f7bd32971de3dca9ff1c08ca6a3759
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-06-07 02:36:18 -0600

    Se agregó el gitignore y parece que hay uan configuración incial adecuada.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hola mundo!')
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.add(ft.Text("Hello, World!"))
+    page.add(ft.Text("This is a test!"))
+    page.add(ft.Text("This is a test!"))
+
+ft.app(main)
