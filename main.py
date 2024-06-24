8718d48af91e270d42f5f4b147da6b2abea7dc6f
Carlos Alberto Baltazar Hinojosa <carval.17080998@outlook.es>
2024-06-24 17:09:25 -0600

    Login view estable, aún falta crear el manejador de evento de logueo

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ def main(page: ft.Page):
 
     # TODO: Verificar la base de datos para ver si hay un usuario con sesión iniciada. No puede haber más de un usuario con sesión iniciada. Si hay un usuario con sesión iniciada, se devuelve ese usuario y se verifica si el usuario ha indicado que se recuerde su sesión. Si el usuario ha indicado que se recuerde su sesión, se inicia sesión automáticamente. Si el usuario no ha indicado que se recuerde su sesión, se muestra la página de inicio de sesión para que pueda iniciar sesión manualmente.
     
-    page.theme_mode = ft.ThemeMode.LIGHT
-    page.theme = LightTheme
-    page.title = 'Punto de venta'
+    loging_view = LoginView(page)
+    page.views.append(loging_view)
+    # page.views.append(AppView(page))
+    page.go(str(loging_view.route))
 
-    page.views.append(LoginView(page))
-    page.views.append(AppView(page))
     
 ft.app(main)
--- views/LoginView.py
+++ views/LoginView.py
@@ -1,9 +1,78 @@
 import flet as ft
 
-from users.user  import User
+from users.user             import User
 from views.AppView          import AppView
-from users.authenticator    import Autenticator
 from theme.theme_config     import LightTheme, DarkTheme
+
+
+class LoginForm(ft.Card):
+    
+    HEIGHT       = 50
+    ELEVATION    = 15
+    WIDTH        = 300
+    BORDER       = ft.InputBorder.NONE
+    INPUT_FILTER = ft.InputFilter(regex_string=r'^[a-z0-9]*$') # Only lowercase letters and numbers
+    STYLE        = ft.ButtonStyle(shape={'': ft.RoundedRectangleBorder(radius=8)})
+
+    def __init__(self, on_login_click=None, on_close_click=None):
+        super().__init__()
+        self.txt_field_username = self._create_text_field('Usuario', 'person')
+        self.txt_field_password = self._create_text_field('Contraseña', 'lock', password=True, can_reveal_password=True)
+        btn_login = self._create_button('Iniciar sesión', on_login_click)
+        btn_close = self._create_button('Cerrar', on_close_click)
+        self.content = ft.Container(
+            content=ft.Column(
+                controls=[
+                    ft.Lottie(
+                        src='https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json',
+                        repeat=True,
+                        # reverse=False,
+                        animate=True,
+                    ),
+                    ft.Text('Inicio de sesión', size=24, weight=ft.FontWeight.BOLD, color=DarkTheme.primary_color),
+                    ft.Text('Ingresa tus credenciales de acceso.', size=13,),
+                    self.txt_field_username,
+                    self.txt_field_password,
+                    btn_login,
+                    btn_close,
+                ],
+                alignment=ft.MainAxisAlignment.START,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=12,
+            ),
+            padding=10,
+            border_radius=15,
+        )
+
+    @property
+    def username(self) -> str | None:
+        return self.txt_field_username.value
+    
+    @property
+    def password(self) -> str | None:
+        return self.txt_field_password.value
+
+    def _create_text_field(self, label: str, icon: str, password: bool = False, can_reveal_password: bool = False) -> ft.TextField:
+        return ft.TextField(
+            label=label,
+            icon=icon,
+            width=self.WIDTH,
+            height=self.HEIGHT,
+            border=self.BORDER,
+            input_filter=self.INPUT_FILTER,
+            password=password,
+            can_reveal_password=can_reveal_password,
+        )
+    
+    def _create_button(self, label: str, on_click=None) -> ft.ElevatedButton:
+        return ft.ElevatedButton(
+            text=label,
+            style=self.STYLE,
+            width=self.WIDTH,
+            height=self.HEIGHT,
+            elevation=self.ELEVATION,
+            on_click=on_click,
+        )
 
 
 class LoginView(ft.View):
@@ -11,102 +80,39 @@ class LoginView(ft.View):
     def __init__(self, page: ft.Page):
         super().__init__()
         self.route = 'login/'
-        self.__app_view = AppView(page)
-        self.txt_field_username = ft.TextField(
-            label='Nombre de usuario',
-            icon='person',
-            width=300,
-            height=50,
-            border=ft.InputBorder.NONE,
-            input_filter=ft.InputFilter(
-                # Sólo permitir letras minusculas y números, pero no caracteres especiales
-                regex_string=r'^[a-z0-9]*$',
-            )
-        )
 
-        self.txt_field_password = ft.TextField(
-            label='Contraseña',
-            icon='lock',
-            width=300,
-            height=50,
-            password=True,
-            can_reveal_password=True,
-            border=ft.InputBorder.NONE,
-            input_filter=ft.InputFilter(
-                # Sólo permitir letras minusculas y números, pero no caracteres especiales
-                regex_string=r'^[a-z0-9]*$',
-            )
-        )
-
-        btn_login = ft.ElevatedButton(
-                content=ft.Text('Iniciar sesión', size=13, weight=ft.FontWeight.BOLD),
-                style=ft.ButtonStyle(
-                    shape={'': ft.RoundedRectangleBorder(radius=8)},
-                ),
-                width=300,
-                height=42,
-                elevation=15,
-                on_click=self.handle_on_login_click,
-        )
-        
-        btn_close = ft.ElevatedButton(
-            content=ft.Text('Salir', size=13, weight=ft.FontWeight.BOLD),
-                style=ft.ButtonStyle(
-                    shape={'': ft.RoundedRectangleBorder(radius=8)},
-            ),
-            width=300,
-            height=42,
-            elevation=15,
-            on_click=lambda e: page.window.close(),
-        )
-
-        content = ft.Card(
-            width=400,
-            expand=True,
-            content=ft.Container(
-                content=ft.Column(
-                    controls=[
-                        ft.Lottie(
-                            src='https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json',
-                            repeat=True,
-                            # reverse=False,
-                            animate=True,
-                        ),
-                        ft.Text('Inicio de sesión', size=24, weight=ft.FontWeight.BOLD, color=DarkTheme.primary_color),
-                        ft.Text('Ingresa tus credenciales de acceso.', size=13,),
-                        self.txt_field_username,
-                        self.txt_field_password,
-                        btn_login,
-                        btn_close,
-                    ],
-                    alignment=ft.MainAxisAlignment.START,
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    spacing=12,
-                ),
-                padding=10,
-                border_radius=15,
-            )
-        )
-        self._set_login_page_config(page, content)
-
-    def _set_login_page_config(self, page: ft.Page, content):
-        page.window.width = 400
-        page.window.height = 580
-        page.theme_mode = ft.ThemeMode.LIGHT
-        page.theme = LightTheme
-        page.vertical_alignment = ft.MainAxisAlignment.CENTER
-        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-        page.window.frameless = True
-        page.add(content)
+        page.window.width           = 340
+        page.window.height          = 500
+        page.window.frameless       = True
+        page.theme                  = LightTheme
+        page.theme_mode             = ft.ThemeMode.LIGHT
+        page.vertical_alignment     = ft.MainAxisAlignment.CENTER
+        page.horizontal_alignment   = ft.CrossAxisAlignment.CENTER
         page.window.center()
 
-    def _get_txt_fields_values(self):
-        return {
-            'username': str(self.txt_field_username.value),
-            'password': str(self.txt_field_password.value),
-        }
+        self.controls = [
+            ft.Container(
+                content=LoginForm(
+                    on_login_click=self.handler_on_login_click,
+                    on_close_click=self.handler_on_close_click,
+                ),
+                alignment=ft.alignment.center,
+                expand=True,
+                expand_loose=True,
+            )
+        ]
     
-    def create_snackbar(self, message: str):
+    def handler_on_login_click(self, event: ft.ControlEvent):
+        pass
+
+    def handler_on_close_click(self, event: ft.ControlEvent):
+        page: ft.Page = event.page
+        page.window.close()
+
+
+    
+    
+    def create_snackbar_message(self, message: str):
         return ft.SnackBar(
             content=ft.Row([ft.Text(message, size=13, weight=ft.FontWeight.BOLD)], alignment=ft.MainAxisAlignment.CENTER),
             width=300,
@@ -115,17 +121,9 @@ class LoginView(ft.View):
         )
     
     def show_not_found_user_msg(self, msg: str, event: ft.ControlEvent):
-        snackbar = self.create_snackbar(msg)
+        snackbar = self.create_snackbar_message(msg)
         snackbar.open = True
         page: ft.Page = event.page
         page.overlay.append(snackbar)
         page.update()
     
-    def handle_on_login_click(self, event: ft.ControlEvent):
-        user = self._get_txt_fields_values()
-        authenticator = Autenticator(username=user['username'], password=user['password'])
-        result = authenticator.authenticate()
-        if isinstance(result, User):
-            self.__app_view.user = result
-        else:
-            self.show_not_found_user_msg(msg=str(result), event=event)
